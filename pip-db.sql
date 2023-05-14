de5908dbacf84967e6e6327fb13ffd061ca56911
Lids <128116103+Lids02@users.noreply.github.com>
2023-05-14 16:13:24 +0100

    Update pip-db.sql
    Movie_Info table created

--- pip-db.sql
+++ pip-db.sql
@@ -9,3 +9,17 @@ first_name VARCHAR(50),
 username VARCHAR(50),
 password VARCHAR(50)
 );
+
+CREATE TABLE movie_info (
+  ID INT NOT NULL,
+  Title VARCHAR (50) NOT NULL,
+  Description VARCHAR (100) NOT NULL,
+  Director VARCHAR (50) NOT NULL,
+  Length FLOAT NOT NULL,
+  Type VARCHAR (50) NOT NULL,
+  Image BLOB,
+  Release_date DATE,
+  Rating FLOAT NOT NULL,
+  Available_Platform VARCHAR (50),
+  PRIMARY KEY (ID)
+);
