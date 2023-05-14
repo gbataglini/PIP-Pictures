fcf80bf49db7de9da5b3b5ab0dd831ac2b5dbd61
Lids <128116103+Lids02@users.noreply.github.com>
2023-05-14 16:13:44 +0100

    Update pip-db.sql

--- pip-db.sql
+++ pip-db.sql
@@ -2,12 +2,12 @@ CREATE DATABASE pipdb;
 USE pipdb;
 
 CREATE TABLE user_info (
-user_id INT NOT NULL AUTO_INCREMENT KEY,
-email VARCHAR(50),
-last_name VARCHAR(50),
-first_name VARCHAR(50),
-username VARCHAR(50),
-password VARCHAR(50)
+  user_id INT NOT NULL AUTO_INCREMENT KEY,
+  email VARCHAR(50),
+  last_name VARCHAR(50),
+  first_name VARCHAR(50),
+  username VARCHAR(50),
+  password VARCHAR(50)
 );
 
 CREATE TABLE movie_info (
