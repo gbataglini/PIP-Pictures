2bb148fa091df86f3e684741249bdb6471f1d229
Lids <128116103+Lids02@users.noreply.github.com>
2023-05-14 16:16:19 +0100

    Delete user_stats.sql
    this tables was added to pip db therefore this file deleted

--- user_stats.sql
+++
@@ -1,9 +0,0 @@
-USE pipdb;
-
-CREATE TABLE user_stats (
-    user_id INT NOT NULL,
-    film_id INT NOT NULL,
-    user_rating FLOAT NOT NULL,
-    status ENUM('watched', 'not watched') NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user_info(user_id)
-);
