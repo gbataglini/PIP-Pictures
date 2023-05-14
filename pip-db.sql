8396b89ef03544ca073b117b801f1be0db757b74
Lids <128116103+Lids02@users.noreply.github.com>
2023-05-14 16:15:32 +0100

    Update pip-db.sql
    Database with tables user stats added

--- pip-db.sql
+++ pip-db.sql
@@ -23,3 +23,11 @@ CREATE TABLE movie_info (
   Available_Platform VARCHAR (50),
   PRIMARY KEY (ID)
 );
+
+CREATE TABLE user_stats (
+  user_id INT NOT NULL,
+  film_id INT NOT NULL,
+  user_rating FLOAT NOT NULL,
+  status ENUM('watched', 'not watched') NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user_info(user_id)
+);
